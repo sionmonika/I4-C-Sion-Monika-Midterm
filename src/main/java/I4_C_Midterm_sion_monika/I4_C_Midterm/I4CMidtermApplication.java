a723db343102f927741493d414cb2c40d28f559c
sionmonika <sionmonoco@gmail.com>
2025-05-07 15:11:12 -0400

    initail commite

---
+++ src/main/java/I4_C_Midterm_sion_monika/I4_C_Midterm/I4CMidtermApplication.java
@@ -0,0 +1,13 @@
+package I4_C_Midterm_sion_monika.I4_C_Midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I4CMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I4CMidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/I4_C_Midterm_sion_monika/I4_C_Midterm/I4CMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package I4_C_Midterm_sion_monika.I4_C_Midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I4CMidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
